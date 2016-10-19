173cbadcd3348875b345629b1646e67589a22805
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2016-10-19 12:58:08 +0300

    LastChanges

---
+++ Mathematics.Web/App_Start/NinjectWeb.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="NinjectWeb.cs" company="Business Management System Ltd.">
+//     Copyright 2016 (c) Business Management System Ltd.. All rights reserved.
+// </copyright>
+// <author>Nikolay.Kostadinov</author>
+//-----------------------------------------------------------------------
+    /// <summary>
+    /// Summary description for $classname$
+    /// </summary>
+
+using Mathematics.Web.App_start;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWeb), "Start")]
+namespace Mathematics.Web.App_start
+{
+        using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+        using Ninject.Web;
+        using Ninject.Web.Common;
+
+        public static class NinjectWeb
+        {
+            /// <summary>
+            /// Starts the application
+            /// </summary>
+            public static void Start()
+            {
+                DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            }
+        }
+    }
---
+++ Mathematics.Web/Models/EquationMemberViewModel.cs
@@ -0,0 +1,27 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EquationMemberViewModel.cs" company="Business Management System Ltd.">
+//     Copyright 2016 (c) Business Management System Ltd.. All rights reserved.
+// </copyright>
+// <author>Nikolay.Kostadinov</author>
+//-----------------------------------------------------------------------
+namespace Mathematics.Web.Models
+{
+    #region Usings
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using Application.Models;
+    using Infrastructure.Mapping;
+
+    #endregion
+
+    /// <summary>
+    /// Summary description for $classname$
+    /// </summary>
+    public class EquationMemberViewModel:IMapFrom<EquationMember>
+    {
+        public int Value { get; set; }
+        public Operations Operation { get; set; }
+    }
+}
